ed01bde525a6b09cda96cf479b59ea990c0af852
James <gotnospirit@gmail.com>
2017-09-03 15:55:17 +0200

    endpoint++

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 var active = false
 var animating = false
+export var final_frame = 1
 
 func _ready():
-	get_node("body").connect("body_enter", self, "_hit")
+	get_node("body").connect("body_enter", self, "_enter")
 	get_node("body/sprite").set_frame(0)
 
-func set_active():
+func on_spin_end():
 	active = true
 	animating = false
-	get_node("body/sprite").set_frame(1)
+	get_node("body/sprite").set_frame(final_frame)
 
-func _hit(body):
+func _enter(body):
 	if !active && !animating && body.is_in_group("hero"):
 		animating = true
-		get_node("animation").play("hit")
+		get_node("animation").play("spin")
